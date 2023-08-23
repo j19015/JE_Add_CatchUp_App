158f928a7b1dd882caf163c8adcb05ef9b277b18
j19015 <j19015@sangi.jp>
2023-08-23 21:08:14 +0900

    controllerの newアクションの記述を変更 & paramsの記述も変更

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,6 +13,7 @@ class ProjectsController < ApplicationController
   # GET /projects/new
   def new
     @project = Project.new
+    @project.tasks.build 
   end
 
   # GET /projects/1/edit
@@ -65,6 +66,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:name, :description)
+      params.require(:project).permit(:name, :description, tasks_attributes: [:id, :description, :done, :_destroy])
     end
 end
