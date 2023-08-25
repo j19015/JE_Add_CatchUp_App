f9c45f11d696c4c0d660879dfcc75921d38d0a15
j19015 <j19015@sangi.jp>
2023-08-25 17:27:22 +0900

    scaffoldでEventモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_23_102155) do
+ActiveRecord::Schema.define(version: 2023_08_25_082334) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
